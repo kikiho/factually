b1e204b319a822e6e10e0a52be9679adb384f6ff
Kiki Ho <kiki@cronometer.com>
2019-07-21 22:30:02 -0700

    forked from original AI4Good project git; preliminary refactoring, restructuring and gentle clean up to prepare for depolyment

--- mythbuster.py
+++ mythbuster.py
@@ -10,14 +10,12 @@ import numpy as np
 # -*- coding: utf-8 -*-
 
 def give_claim(comment):
-    df=pd.read_csv('C:\\Users\\bosil\\Documents\\webapp\\mythfact.csv')
+    df=pd.read_csv('data/mythfact.csv')
     df.columns = ['Keyword','Claim','False']
 
     keyword_bank = df.Keyword.unique().tolist()
 
-    print (comment.lower())
     for keyword in keyword_bank:
-      print (keyword)
       if (keyword in comment.lower()):
           new_df = (df.loc[df['Keyword'].str.contains(keyword),:])
           return new_df.iloc[0]['Claim']
